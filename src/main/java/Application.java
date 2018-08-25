054e22b26403cbb799805481a239dc56d29c1078
cseby92 <cseby92@gmail.com>
2018-08-25 19:22:04 +0200

    Remove unused Mockito dewpendency

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     public static void main(String[] args) {
         VacationPathCalculator calculator = new VacationPathCalculator();
-        String path = "a => b\nb => b";
+        String path = "a => b\nc => b";
         try {
             System.out.println(calculator.calculateOptimalPath(path));
         } catch (VacationPathCalculatorException e) {
